017fc1312cafec965529b21600619213b7c53338
Connie Ip <connie.ip@cargosmart.com>
2018-11-11 15:42:08 +0800

    ArrayTest

--- src/test/java/com/cultivation/javaBasic/showYourIntelligence/MyStack.java
+++ src/test/java/com/cultivation/javaBasic/showYourIntelligence/MyStack.java
@@ -2,6 +2,9 @@ package com.cultivation.javaBasic.showYourIntelligence;
 
 import sun.reflect.generics.reflectiveObjects.NotImplementedException;
 
+import java.lang.reflect.Array;
+import java.util.Arrays;
+
 @SuppressWarnings({"unused", "FieldCanBeLocal"})
 public class MyStack {
     private int[] storage;
@@ -26,7 +29,8 @@ public class MyStack {
 
         // TODO: Please push the value into the storage here.
         // <--start
-        throw new NotImplementedException();
+        storage[count]=value;
+        count++;
         // --end-->
     }
 
@@ -36,7 +40,8 @@ public class MyStack {
         // TODO: Please create a new array of size newCapacity. And update related fields
         // TODO: You SHOULD NOT USE COLLECTIONS OTHER THAN ARRAY.
         // <--start
-        throw new NotImplementedException();
+        storage=Arrays.copyOf(storage,newCapacity);
+        capacity=newCapacity;
         // --end-->
     }
 
@@ -54,6 +59,10 @@ public class MyStack {
     private int pop() {
         // TODO: Please pop one element from the array.
         // <--start
+        if(storage.length>0) {
+            count--;
+            return storage[count];
+        }
         // --end-->
 
         throw new UnsupportedOperationException("Stack is empty.");
